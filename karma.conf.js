85c7554c55314e208dd6fd100b1f3e8c375b7362
Jhonatan Alarcon <jhonatan.alarcon.ariza@gmail.com>
2018-09-04 22:33:36 -0500

    Initial commit

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,7 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-require('phantomjs-prebuilt').path = './node_modules/.bin/phantomjs';
+//require('phantomjs-prebuilt').path = './node_modules/.bin/phantomjs';
 
 module.exports = function (config) {
     config.set({
@@ -19,7 +19,8 @@ module.exports = function (config) {
             clearContext: false // leave Jasmine Spec Runner output visible in browser
         },
         coverageIstanbulReporter: {
-            dir: require('path').join(__dirname, 'coverage'), reports: [ 'html', 'lcovonly' ],
+            dir: require('path').join(__dirname, 'coverage'),
+            reports: [ 'html', 'lcovonly' ],
             fixWebpackSourcePaths: true
         },
         
